e8552698d04994fd76510e53e7c090bad1868477
Eduardo Betancourt <ebhdz@hotmail.com>
2020-11-30 11:39:22 -0600

    imported libraries

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,12 +2,15 @@ import streamlit as st
 import os
 from tensorflow.keras.models import load_model
 import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup as bsp
 
 
 #'https://s3-us-west-2.amazonaws.com/flx-editorial-wordpress/wp-content/uploads/2018/03/13153742/RT_300EssentialMovies_700X250.jpg'
 
 
-
+# General Styling for Webpage
 CSS = """
 
 body {
@@ -17,31 +20,49 @@ body {
 }
     """
 
+header_img_url = 'https://s3-us-west-2.amazonaws.com/flx-editorial-wordpress/wp-content/uploads/2018/03/13153742/RT_300EssentialMovies_700X250.jpg'
+
+HEADER_CSS = f"""
+#header {{
+    background-image: url('{header_img_url}');
+    width: 100%;
+    height: 200px;
+}}
+.center {{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}}
+#header h1 {{
+  color: white;
+}}
+"""
+
+HEADER_HTML = f"""
+<style>
+    {HEADER_CSS}
+</style>
+
+<div id='header' class='center'>
+    <h1>
+        SPLOCKED!
+    </h1>
+</div>
+"""
+
+st.write(HEADER_HTML, unsafe_allow_html=True)
+
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 
-st.markdown("# **SPLOCKED !**")
-st.markdown("### *Check if your friend's comment contain spoiler!\
- Or, supply an URL and find out if the comments contain any spoilers!*")
+st.markdown("### *Check your movie's reviews without spoilers!*")
 
 
 def main():
 
   #model = load_model('\models\saved_model.pb')
-  text = st.text_input("Type the comment here: ", "OMG, I can't believe McDreamy died!")
 
-  url = st.text_input("Or...type the URL here: ",\
+  url = st.text_input("Type the IMDB movie review URL here: ",\
    "https://www.imdb.com/title/tt8134470/reviews?ref_=tt_urv")
-
-
-  choice = st.radio('Are you sure you would like to know?', ('Yes', 'No'))
-  if choice == 'Yes':
-    pred = model.predict(X)
-    if pred[1] > 0.5:
-      st.write("SPOILER ALLERT!!!!")
-    else:
-      st.write("Nhá, you can read it!")
-  else:
-    st.write("Why are you here then?!")
 
 if __name__ == "__main__":
     #df = read_data()
