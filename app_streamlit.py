c576a888bcfb56abd4a118d90dc698a37b6a6b7e
Eduardo Betancourt <ebhdz@hotmail.com>
2020-12-01 12:31:46 -0600

    Made a review card

--- app_streamlit.py
+++ app_streamlit.py
@@ -71,6 +71,100 @@ st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 
 st.markdown("### *Check your movie's reviews without spoilers!*")
 
+
+REVIEW_CSS = """
+summary::-webkit-details-marker {
+ color: #00ACF3;
+ font-size: 100%;
+ margin-right: 2px;
+ position: absolute;
+ top: 50px;
+ right: 5px;
+ z-index: 100;
+}
+summary:focus {
+  outline-style: none;
+}
+article > details > summary {
+  font-size: 28px;
+  margin-top: 16px;
+}
+details > p {
+  margin-left: 24px;
+}
+details details {
+  margin-left: 36px;
+}
+details details summary {
+  font-size: 16px;
+}
+.title {
+  color: black;
+  z-index: 1;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+}
+
+.green {
+  background-color: #99EA9A;
+  border: 5px solid #78B779;
+}
+
+.relative {
+  position: relative;
+  z-index: 10
+}
+.bottom {
+  position: absolute;
+  top: 40px;
+  right: 25px;
+}
+.no-margin {
+  margin: 0px 0px 0px 0px;
+}
+"""
+
+REVIEW_CARD = f"""
+<style>
+{REVIEW_CSS}
+</style>
+<div class='relative'>
+  <details>
+    <summary>
+      <div class='title green'>
+          <div>
+            <div>
+              <div><em>Spoiler Proba<em></div>
+              <div>Comment Summary</div>
+            </div>
+          </div>
+          <div class="bottom mr-5 ">
+            <p>read full review</p>
+          </div>
+      </div>
+    </summary>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Sed non eleifend libero. Nullam quis dolor vel
+      lorem auctor imperdiet eu pulvinar erat.
+      Vestibulum venenatis nisl non felis sollicitudin aliquam.
+      Praesent aliquam maximus sem, et vestibulum orci pulvinar eget.
+      ras consectetur faucibus justo, vitae vulputate neque. Aenean
+      eu metus arcu. Proin porta mi mi, sit amet bibendum augue finibus
+      sit amet. Pellentesque vestibulum dolor nec mollis bibendum.
+      In quis quam euismod quam condimentum elementum.
+    </p>
+  </details>
+</div>
+"""
+
+
+#st.write(REVIEW_CARD, unsafe_allow_html=True)
+st.write(REVIEW_CARD, unsafe_allow_html=True)
+
 def main():
 
   model = load_model(os.path.join('model_baseline'))
